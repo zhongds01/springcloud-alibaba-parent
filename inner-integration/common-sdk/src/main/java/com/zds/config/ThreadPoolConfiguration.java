2faeddbb04294bc761d9d6b0d4890feb3e812fda
zhongds01 <zhongds01@163.com>
2022-04-26 22:41:43 +0800

    补充对线程池中maxPoolSize,CorePoolSize等参数理解

--- inner-integration/common-sdk/src/main/java/com/zds/config/ThreadPoolConfiguration.java
+++ inner-integration/common-sdk/src/main/java/com/zds/config/ThreadPoolConfiguration.java
@@ -21,17 +21,23 @@ public class ThreadPoolConfiguration {
     @Value("${spring.thread.pool.corePoolSize:10}")
     private Integer corePoolSize;
 
+    @Value("${spring.thread.pool.queueCapacity:20}")
+    private Integer queueCapacity;
+
+    @Value("${spring.thread.pool.keepAliveSeconds:300}")
+    private Integer keepAliveSeconds;
+
     @Bean
     public AsyncTaskExecutor asyncTaskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        // 线程池核心线程的数量，默认值为1（不能被重用的原因）
+        // 线程池核心线程的数量，默认值为1。核心线程数达到最大值后，其余线程会进入线程队列中等待，直到核心线程得到释放。
         executor.setCorePoolSize(corePoolSize);
         // 线程池维护线程的最大数量，只有当核心线程都被用完并且缓冲队列满后，才会开始申超过请核心线程数的线程，默认值为Integer.MAX_VALUE
         executor.setMaxPoolSize(maxPoolSize);
-        // 缓冲队列
-        executor.setQueueCapacity(30);
+        // 缓冲队列，核心线程达到最大值时，新线程进入线程队列的队列大小
+        executor.setQueueCapacity(queueCapacity);
         // 超出核心线程数外的线程在空闲时候的最大存活时间，默认为60秒
-        executor.setKeepAliveSeconds(60);
+        executor.setKeepAliveSeconds(keepAliveSeconds);
         // 线程名称前缀
         executor.setThreadNamePrefix("Async-thread-");
         executor.setTaskDecorator(new ThreadContextDecorator());
@@ -39,7 +45,7 @@ public class ThreadPoolConfiguration {
         executor.setWaitForTasksToCompleteOnShutdown(true);
         // waitForTasksToCompleteOnShutdown的等待的时长，默认值为0，即不等待
         executor.setAwaitTerminationSeconds(60);
-        // 当没有线程可以被使用时的处理策略（拒绝任务），默认策略为abortPolicy
+        // 当没有线程可以被使用时的处理策略（拒绝任务），默认策略为abortPolicy。当核心线程、缓冲队列、最大线程都达到最大值时的处理策略
         /**
          * callerRunsPolicy：用于被拒绝任务的处理程序，直接在execute方法的调用线程中运行被拒绝的任务；如果执行程序已关闭，则会丢弃该任务
          * abortPolicy：直接抛出java.util.concurrent.RejectedExecutionException异常
